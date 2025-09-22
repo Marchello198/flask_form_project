1aa85e708556471ca79b47b4dd5b578f541b0263
Marchelo <denismarchenko13132@gmail.com>
2025-09-22 20:31:48 +0300

    Создание формы и обработка POST и GET запросов

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,28 @@
 from flask import render_template, request, redirect, url_for
 from app import app
 
+
 @app.route("/")
-def form():
-    return render_template("form.html")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/submit", methods=["POST", "GET"])
 def submit():
     if request.method == "POST":
         name = request.form.get("name")  # Получаем имя из формы
         email = request.form.get("email")  # Получаем email из формы
-        color = request.form.get("color")
-        profession = request.form.get("profession")
-        hobbies = request.form.getlist("hobbies")
-        level = request.form.get("level")
-        return render_template("result.html", name=name, email=email, color=color, profession=profession, hobbies=hobbies, level=level)
+        message = request.form.get("message")  # Получаем сообщение из формы
+        return render_template("contact.html", name=name, email=email, message=message)
     else:
-        return redirect(url_for("form"))  # Если запрос GET, возвращаем на форму
+        return redirect(url_for("contact"))  # Если запрос GET, возвращаем на форму
 
